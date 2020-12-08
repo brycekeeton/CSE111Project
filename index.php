a5c38cafffecc3c7d63b6c1b28c139f95dac4050
brycekeeton <72243495+brycekeeton@users.noreply.github.com>
2020-12-08 09:56:23 -0800

    all done except gameplay upload still broken

--- index.php
+++ index.php
@@ -16,21 +16,26 @@
 	
 	if($_SERVER["REQUEST_METHOD"] == "POST"){
 		echo('<u>Query: <b>' . $_POST['queryType'] . '</b></u><br>');
-		 if($_POST['querytype'] == "Search Query")
+		 if($_POST['queryType'] == "Search Query")
 		 	rdmQuery($_POST['rdmQuery']);
 		else;
 
 	}
 
-/*	function rdmQuery($rdmQuery){
+	function rdmQuery($rdmQuery){
 		$location = 'data.sqlite';
 		$db = new SQLite3($location);
-		$sql = '" . $rdmQuery . "'
+		$sql = $rdmQuery;
 		$result = $db->query($sql);
-		echo("Here ya go!");
+		echo("<div>");
+		while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+			echo(implode(" | ",$row) . "<br>");
+		}
+		echo("</div>");
+		
 		unset($db);
 	}
-*/ 
+ 
 	// close database
 ?>
 
@@ -40,23 +45,27 @@
 		<style>
 			div {
 				height: 200px;
-				width: 500px;
+				width: 1000px;
 				overflow-y: auto;
 				border: 1px solid black;
 			}
 		</style>
 	</head>
 	<body>
+		<hr>
 		Carlos' and Bryce's Freaking Sweet Game Rating Site!<br>
 		<hr>
 		<table><tr><td>
 		<b>Sign Up, View Users, or Delete Your Account:</b></td><td> [<a href="pages/Users.php">User Page</a>]<br></td></tr>
 		<tr><td><b>View Games, Edit the Database, or View and Write Reviews:</b></td><td> [<a href="pages/Games.php">Games Page</a>]
 		<tr><td><b>Generate and View Recommendations:</b></td><td> [<a href="pages/Recommendations.php">Recommendation Page</a>]
-		<br></td></tr></table>
+		<br></td></tr></table><br>
 		
-		Have a query in mind already? Input it here! <input type="text" name="rdmQuery"> <br>
-		<input type="submit" name="queryType" value= "Search Query">
+		Have a query in mind already? Input it here! 
+		<form method="post">
+			<textarea name="rdmQuery"></textarea> <br>
+			<input type="submit" name="queryType" value= "Search Query">
+		</form>
 		
 	</body>
 
